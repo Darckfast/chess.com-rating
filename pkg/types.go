e4d59205ce974b6da11138966e9eeb05f43420e4
darckfast <21337578+Darckfast@users.noreply.github.com.>
2025-08-06 20:04:12 -0300

    chore: removed unused fields

--- pkg/types.go
+++ pkg/types.go
@@ -6,35 +6,7 @@ type ChessStats struct {
 	Stats []struct {
 		Key   string `json:"key"`
 		Stats struct {
-			Rating                json.RawMessage `json:"rating"`
-			HighestRating         int             `json:"highest_rating"`
-			HighestRatingDate     string          `json:"highest_rating_date"`
-			RatingTimeChangeDays  int             `json:"rating_time_change_days"`
-			RatingTimeChangeValue int             `json:"rating_time_change_value"`
-			TotalGameCount        int             `json:"total_game_count"`
-			TotalWinCount         int             `json:"total_win_count"`
-			TotalLossCount        int             `json:"total_loss_count"`
-			TotalDrawCount        int             `json:"total_draw_count"`
-			AvgOpponentRating     int             `json:"avg_opponent_rating"`
-			TimeoutPercent        int             `json:"timeout_percent"`
-			TimeoutDays           int             `json:"timeout_days"`
-			TotalInProgressCount  int             `json:"total_in_progress_count"`
-			AvgMoveTime           float64         `json:"avg_move_time"`
-			LastDate              string          `json:"last_date"`
+			Rating json.RawMessage `json:"rating"`
 		} `json:"stats"`
-		GameCount  int    `json:"gameCount"`
-		LastPlayed bool   `json:"lastPlayed"`
-		LastDate   string `json:"lastDate,omitempty"`
 	} `json:"stats"`
-	LastType string `json:"lastType"`
-	Versus   struct {
-		Total int `json:"total"`
-	} `json:"versus"`
-	RatingOnlyStats []string    `json:"ratingOnlyStats"`
-	OfficialRating  interface{} `json:"officialRating"`
-	LessonLevel     struct {
-		Icon     string `json:"icon"`
-		Name     string `json:"name"`
-		Progress int    `json:"progress"`
-	} `json:"lessonLevel"`
 }
